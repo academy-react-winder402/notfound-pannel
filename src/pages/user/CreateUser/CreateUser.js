c26764d8fa03120ffb7db7be9c80e2249bae15a0
mehdi <mehdi@gamil.com>
2024-05-30 09:55:50 +0330

    create user

--- src/pages/user/CreateUser/CreateUser.js
+++ src/pages/user/CreateUser/CreateUser.js
@@ -1,8 +1,94 @@
+import { Form, Formik } from 'formik'
 import React from 'react'
+import InputFloating from '../../../component/InputFloating/InputFloating'
+import { Button, Col, Input, Row } from 'reactstrap'
+import http from '../../../core/services/interceptore'
 
 const CreateUser = () => {
+  const handleCreateUser =async (values) => {
+    const res=await http.post('/User/CreateUser',values);
+
+    console.log(res);
+  }
+  
   return (
-    <div>CreateUser</div>
+    <div>
+      <Formik onSubmit={handleCreateUser} initialValues={{lastName : '' , firstName : '' , gmail : '' , password : '' , phoneNumber : '' , isStudent : true , isTeacher : false}}>
+        {({values , handleChange})=>(
+          <Form>
+          <Row>
+            <Col md='5' sm='12'>
+              <div className='form-floating'>
+                <Input name='lastName' value={values.lastName} onChange={handleChange} type='text' id='floatingInput' placeholder='lastName' />
+                <label htmlFor='floatingInput'>lastName</label>
+              </div>
+            </Col> 
+
+            <Col md='1'></Col>
+
+            <Col md='5' sm='12'>
+              <div className='form-floating'>
+                <Input name='firstName' value={values.firstName} onChange={handleChange} type='text' id='floatingInput' placeholder='firstName' />
+                <label htmlFor='floatingInput'>firstName</label>
+              </div>
+            </Col>           
+          </Row>
+
+          
+
+          <Row style={{marginTop:'50px'}}>
+            <Col md='5' sm='12'>
+              <div className='form-floating'>
+                <Input name='gmail' value={values.gmail} onChange={handleChange} type='text' id='floatingInput' placeholder='gmail' />
+                <label htmlFor='floatingInput'>gmail</label>
+              </div>
+            </Col> 
+
+            <Col md='1'></Col>
+
+            <Col md='5' sm='12'>
+              <div className='form-floating'>
+                <Input name='password' value={values.password} onChange={handleChange} type='password' id='floatingInput' placeholder='password' />
+                <label htmlFor='floatingInput'>password</label>
+              </div>
+            </Col>           
+          </Row>
+
+
+          <Row style={{marginTop:'50px'}}>
+            <Col md='5' sm='12'>
+              <div className='form-floating'>
+                <Input name='phoneNumber' value={values.phoneNumber} onChange={handleChange} type='text' id='floatingInput' placeholder='phoneNumber' />
+                <label htmlFor='floatingInput'>phoneNumber</label>
+              </div>
+            </Col> 
+
+            <Col md='1'></Col>
+
+            <Col md='2' sm='12'>
+              <div className='form-floating'>
+                <Input name='isStudent' value={values.isStudent} onChange={handleChange} type='checkbox' id='floatingInput' placeholder='isStudent' />
+                <label htmlFor='floatingInput'>isStudent</label>
+              </div>
+            </Col> 
+            
+           <Col md='1'></Col>
+
+            <Col md='2' sm='12'>
+              <div className='form-floating'>
+                <Input name='isTeacher' value={values.isTeacher} onChange={handleChange} type='checkbox' id='floatingInput' placeholder='isTeacher' />
+                <label htmlFor='floatingInput'>isTeacher</label>
+              </div>
+            </Col>                       
+          </Row>          
+
+
+            <Button type='submit' color='primary'>create</Button>
+          </Form>          
+        )}
+
+      </Formik>
+    </div>
   )
 }
 
--- src/pages/user/UsersList/UsersList.js
+++ src/pages/user/UsersList/UsersList.js
@@ -1,8 +1,103 @@
 import React from 'react'
-
+import { Table } from 'reactstrap'
+import http from '../../../core/services/interceptore'
+import { useQuery } from 'react-query'
 const UsersList = () => {
+
+  const getUserList =async () =>{
+    const res=await http.get('/User/UserMannage?PageNumber=1&RowsOfPage=100&SortingCol=DESC&SortType=InsertDate&Query=&IsActiveUser=true')
+    console.log(res);
+    return res
+  }
+  const {data}=useQuery("getUser",getUserList)
+  data&&console.log(data);
   return (
-    <div>UsersList</div>
+    <div>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>نام</th>
+            <th>نوع کاربر</th>
+            <th> id</th>
+            <th>جنسیت</th>
+            <th>درصد تکمیل اطلاعات</th>
+            <th>ایمیل</th>              
+            <th>شماره تماس</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+
+            </td>
+
+            <td>
+   ngf
+            </td>
+
+            <td>
+            ngf
+            </td>        
+
+            <td>
+              gfhnfh
+            </td>
+
+            <td>
+              nxfg
+            </td>
+
+            <td>
+tyj
+            </td>        
+
+            <td>
+gnzng
+            </td>
+
+            <td>
+rtjhr
+            </td>        
+
+          </tr>
+          <tr>
+            <td>
+
+            </td>
+
+            <td>
+   ngf
+            </td>
+
+            <td>
+            ngf
+            </td>        
+
+            <td>
+              gfhnfh
+            </td>
+
+            <td>
+              nxfg
+            </td>
+
+            <td>
+tyj
+            </td>        
+
+            <td>
+gnzng
+            </td>
+
+            <td>
+rtjhr
+            </td>        
+
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
